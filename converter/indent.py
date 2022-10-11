722b80546cb46f58a0b03cea030e4c5368115d28
UrszulaNeuman <urszula.neuman@gmail.com>
2022-10-11 11:53:54 +0100

    working the same as the master version

--- converter/indent.py
+++ converter/indent.py
@@ -1,13 +1,12 @@
 class Indenter:
-    def __init__(self):
-        self.level = -1
+    def __init__(self, level):
+        self.level = level
 
     def __enter__(self):
-        self.level += 1
         return self
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        self.level -= 1
+        pass
 
     def format_text(self, text):
         return "    " * self.level + text + "\n"
