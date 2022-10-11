f740d7e8e7d096a75af35dd1e54176748ec7db84
UrszulaNeuman <UrszulaNeuman@users.noreply.github.com>
2022-10-11 13:42:30 +0000

    deploy: cce0042b84c77decfac9bef3ca320fbec2dc5ce1

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0+29.gf5b81b8',
+    VERSION: '1.0+34.gcce0042',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
