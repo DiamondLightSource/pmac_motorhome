bff9bc700c759f0c4f48358a284e98046b54e128
UrszulaNeuman <UrszulaNeuman@users.noreply.github.com>
2023-03-24 12:07:53 +0000

    deploy: 1c50436c48083e8376aa595b489766cc0553bc90

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0+82.g38b4755',
+    VERSION: '1.0+84.g1c50436',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
