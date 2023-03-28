d88ac3feffea6a9a53083bc929255e23cfe898f5
UrszulaNeuman <urszula.neuman@gmail.com>
2023-03-28 15:03:08 +0100

    update test

--- tests/unittests/converter/test_motionarea.py
+++ tests/unittests/converter/test_motionarea.py
@@ -3,15 +3,13 @@ from pathlib import Path
 from unittest.mock import patch
 
 from converter.motionarea import MotionArea
+from pmac_motorhome._version_git import __version__
 
 
 class TestMotionArea(unittest.TestCase):
-    @patch("subprocess.check_output")
-    def test_shebang_looks_as_expected(self, mock_subprocess):
+    def test_shebang_looks_as_expected(self):
         # Arrange
-        python_path = "/bin/python"
-        expected_shebang = "#!/bin/env /bin/python"
-        mock_subprocess.return_value = python_path.encode("UTF-8")
+        expected_shebang = "#!/bin/env /dls_sw/prod/python3/RHEL7-x86_64/pmac_motorhome/"+__version__+"/lightweight-venv/bin/python3"
         motionarea = MotionArea(Path("/tmp"))
         # Act
         content = motionarea.get_shebang()
