fce16426036b1e5ead9302665db2c66257c11bbb
UrszulaNeuman <urszula.neuman@gmail.com>
2024-01-11 09:45:04 +0000

    Revert "log info instead of raising error"
    This reverts commit 5e3f073f187bc7dc2c6153353c1350504c34f46c.

--- pmac_motorhome/plc.py
+++ pmac_motorhome/plc.py
@@ -56,11 +56,12 @@ class Plc:
         self.groups: List[Group] = []
         self.motors: "OrderedDict[int, Motor]" = OrderedDict()
         self.generator = PlcGenerator(self.controller)
-        directory_name = self.filepath.parts[0]
-        other_parts = self.filepath.parts[1:]
-        if (any(directory_name in i for i in Path.cwd().parts)):
-            self.filepath = Path(other_parts[0], other_parts[1])
-            log.info(f"New file path {self.filepath} from dir {Path.cwd()}")
+        if not self.filepath.parent.exists():
+            log.error(f"Cant find parent of {self.filepath} from dir {Path.cwd()}")
+            raise ValueError(
+                f"bad file path {self.filepath.parent}\
+                from dir {Path.cwd()}"
+            )
         if (
             self.plc_num < 8  # PLCs 1-8 are reserved
             or self.plc_num > 32  # highest PLC number possible
@@ -88,11 +89,8 @@ class Plc:
 
         # write out PLC
         plc_text = self.generator.render("plc.pmc.jinja", plc=self)
-        try:
-            with self.filepath.open("w") as stream:
-                stream.write(plc_text)
-        except FileNotFoundError: # important when there is one motorhome.py for all controllers 
-            pass
+        with self.filepath.open("w") as stream:
+            stream.write(plc_text)
 
     @classmethod
     def instance(cls) -> "Plc":
